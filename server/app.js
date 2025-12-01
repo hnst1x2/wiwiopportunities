6c11d5f61a49e3a0d4345fe0ad03473c8714c052
hnst1x2 <nohich@gmail.com>
2025-12-02 00:40:27 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
